105a454a39931b2ce4db8c1a565d65b6c7c0056d
BasakOzokcu <basakozokcu@gmail.com>
2020-11-10 12:49:19 -0500

    project is shared in remote repo, xomming more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
  // more stuff
     //mac user and windows user re happy to see committ dialog finally
-
+//i have already publish on github.com
+    //now i m ready to make commint
+    //but i do i need to share again
+    //absoulutely no
+    //its time commint and  just push
 
 }
